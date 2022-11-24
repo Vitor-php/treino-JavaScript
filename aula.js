8549b031056d778bc0478e9489c04d13af52e420
Vitor <moraes.vitor@aluno.ifsp.edu.br>
2022-11-24 00:28:58 -0300

    fix: changes in aula.js

--- aula.js
+++ aula.js
@@ -1,23 +1,44 @@
-const array = [100, 50, 30];
+// let acertos = 0;
+// let i = 0;
+// let x = 0;
+// let y = 0
+// while (i < 10000) {
+//     let x = Math.random()
+//     let y = Math.random()
+// }
+// if (x * x + y * y <= 1.0) {
+//     acertos += 1
+//     i += 1
+// }
+// console.log(4 * acertos / 10000)
 
-console.log(mediaDoAluno(array));
+// acertos <- 0
+// i <- 0
+// while (i < REP)
+//   x <- rand()  (float entre 0 e 1)
+//   y <- rand()
+//   if x * x + y * y <= 1.0
+//     acertos <- acertos + 1
+//   i <- i + 1
+// print(4 * acertos / REP)
 
-function mediaDoAluno(notas) {
-   const media = calcularMedia(notas);
-
-    if (media < 59) return 'E';
-    if (media < 69) return 'D';
-    if (media < 79) return 'C';
-    if (media < 89) return 'B';
-    return 'A';
+// Teste de construtores
+// Transformar esse objeto em uma Constructor Function
+const pessoa = {
+    nome: "Nome Pessoa",
+    idade: 20,
+    andar() {
+        console.log(this.nome + 'andou');
+    }
 }
 
-function calcularMedia(array) {
-    let soma = 0;
-    for (let valor of array) {
-        soma += valor;
+function pessoa(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+    this.andar = function () {
+        console.log(this.nome + 'andou');
     }
-    return soma/(array.length);
+}
 
-} 
-
+const pessoa1 = new pessoa('Vitor', 21);
+const pessoa2 = new pessoa('João', 22);
